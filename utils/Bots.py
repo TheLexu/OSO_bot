175ed6338e80f702186b1d2b3a7126d51803286b
aleks <aleksey05obninsk@gmail.com>
2024-07-21 21:36:08 +0300

    Вопрос+Ответ+Словарь(переделанный)

--- utils/Bots.py
+++ utils/Bots.py
@@ -1,32 +1,48 @@
 import vk_api
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
-#1
-qa_pairs = {
-    "Кто является руководителем кружка?": "Шлапак Никита (ссылка на вк)",
-    "Чем занимается кружок?": "Деятельность кружка делится на три основных направления: изучение архитектуры компьютеров, внутренние проекты института и научно-исследовательская деятельность",
-    "Какой входной порог?": "Зависит от направления. Для курса Computer Science и внутренних проектов института необходимы базовые знания программирования. Для исследовательского направления уже необходимо уверенно владеть языком программирования",
-    "Когда и где проходят занятия?": "Кружок функционирует онлайн. Вся коммуникация (в том числе и актуальное расписание занятий) происходит на дискорд-сервере: ссылка"
-}
+
+from utils.utils import Sender
 
 
-def get_response(user_question):
-    user_question = user_question.strip()
-    response = qa_pairs.get(user_question, "Извините, я не понимаю вопрос.")
-    return response
+class BasicBot(object):
+    HELLO_WORDS = ('start', 'begin', 'начать', 'привет', 'добрый день', 'здравствуйте')
+    qa_pairs = {
+        "Кто является руководителем кружка?": "Шлапак Никита (ссылка на вк)",
+        "Чем занимается кружок?": "Деятельность кружка делится на три основных направления: изучение архитектуры компьютеров, внутренние проекты института и научно-исследовательская деятельность",
+        "Какой входной порог?": "Зависит от направления. Для курса Computer Science и внутренних проектов института необходимы базовые знания программирования. Для исследовательского направления уже необходимо уверенно владеть языком программирования",
+        "Когда и где проходят занятия?": "Кружок функционирует онлайн. Вся коммуникация (в том числе и актуальное расписание занятий) происходит на дискорд-сервере: ссылка"
+    }
+    def __init__(self, token):
+        self.token = token
 
+    def get_response(self, user_question):
+        user_question = user_question.strip().lower()
+        if user_question in self.qa_pairs:
+            return self.qa_pairs[user_question]
+        else:
+            return 'Извините, я не понимаю вашего вопроса'
+    def start(self):
 
-def main():
-    TOKEN = 'vk1.a.twQWhoQKZLg1EGRKrHwX6gyAQTt5FW55JaIE7wTvFWRHa9CWsv9g2HKHhRi9RSZdVR_8UohJ0J-nw1Pma-wekbjojIPkP8w4zSXbRFVmS8UwyMg-jtI-WgH2aJzNBLIImJjPv0Pyz0jf-pYAkye0aoBVFhKK7nRdSPJeR-RGK-cvWan-2hKcn-9BCCAJsRDsaoBjuFOv9OroQmEcITHhtQ'
-    vk_session = vk_api.VkApi(token=TOKEN)
-    longpoll = VkLongPoll(vk_session)
+        vk_session = vk_api.VkApi(token=self.token)
+        vk = vk_session.get_api()
+        longpoll = VkLongPoll(vk_session)
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            user_input = event.text
-            answer = get_response(user_input)
+        sender = Sender(vk)
 
-            vk_session.method('messages.send', {'user_id': event.user_id, 'message': answer,'random_id': 0})
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                text = event.text.lower()
+                user_id = event.user_id
 
-
-if __name__ == "__main__":
-    main()
+                if text in self.HELLO_WORDS:
+                    sender('Добро пожаловать в ОСО!', user_id=user_id)
+                else:
+                    answer = self.get_response(text)
+                    if answer == 'Извините, я не понимаю вашего вопроса':
+                        keyboard = VkKeyboard(one_time=True)
+                        keyboard.add_button('Привет', color=VkKeyboardColor.SECONDARY)
+                        keyboard.add_button('Здравствуйте', color=VkKeyboardColor.NEGATIVE)
+                        sender('Привет!',
+                               user_id=user_id,
+                               keyboard=keyboard.get_keyboard())
