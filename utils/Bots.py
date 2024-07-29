70a3dd0ecfeca94281beac82ef732fa78665ffc9
aleks <aleksey05obninsk@gmail.com>
2024-07-29 19:39:11 +0300

    Вопрос+Ответ+Словарь(переделанный)

--- utils/Bots.py
+++ utils/Bots.py
@@ -1,28 +1,28 @@
+import json
 import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-from utils.utils import Sender
+qa_file = 'qa_pairs.json'
 
 class BasicBot(object):
     HELLO_WORDS = ('start', 'begin', 'начать', 'привет', 'добрый день', 'здравствуйте')
-    qa_pairs = {
-        "кто является руководителем кружка?": "Шлапак Никита (ссылка на вк)",
-        "чем занимается кружок?": "Деятельность кружка делится на три основных направления: изучение архитектуры компьютеров, внутренние проекты института и научно-исследовательская деятельность",
-        "какой входной порог?": "Зависит от направления. Для курса Computer Science и внутренних проектов института необходимы базовые знания программирования. Для исследовательского направления уже необходимо уверенно владеть языком программирования",
-        "когда и где проходят занятия?": "Кружок функционирует онлайн. Вся коммуникация (в том числе и актуальное расписание занятий) происходит на дискорд-сервере: ссылка"
-    }
-    def __init__(self, token):
+
+    def __init__(self, token, qa_file):
         self.token = token
+        self.qa_pairs = self.load_qa_pairs(qa_file)
+
+    def load_qa_pairs(self, qa_file):
+        with open(qa_file, 'r', encoding='utf-8') as file:
+            return json.load(file)
 
     def get_response(self, user_question):
         user_question = user_question.strip().lower()
-        if user_question in self.qa_pairs:
-            return self.qa_pairs[user_question]
-        else:
-            return 'Извините, я не понимаю вашего вопроса'
-    def start(self):
+        response = self.qa_pairs.get(user_question, "Извините, я не понимаю вопрос.")
+        return response
 
+
+    def start(self):
         vk_session = vk_api.VkApi(token=self.token)
         vk = vk_session.get_api()
         longpoll = VkLongPoll(vk_session)
@@ -38,15 +38,4 @@ class BasicBot(object):
                     sender('Добро пожаловать в ОСО!', user_id=user_id)
                 else:
                     answer = self.get_response(text)
-
-                    if answer == "Извините, я не понимаю вашего вопроса":
-                        keyboard = VkKeyboard(one_time=True)
-                        keyboard.add_button('Задать другой вопрос', color=VkKeyboardColor.SECONDARY)
-                        keyboard.add_button('Привет', color=VkKeyboardColor.NEGATIVE)
-
-                        sender(
-                            'Я не понял ваш вопрос. Попробуйте задать его по-другому или выберите одно из предложений.',
-                            user_id=user_id,
-                            keyboard=keyboard.get_keyboard())
-                    else:
-                        sender(answer, user_id=user_id)
+                    sender(answer, user_id=user_id)
